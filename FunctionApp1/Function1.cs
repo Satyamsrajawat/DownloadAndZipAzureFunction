8d408bfa0069b71f4a8dd4aab422525a83936359
satya <satya@DESKTOP-4CHQ7UH>
2022-08-07 12:08:14 +0530

    Searching Files and making Zip

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -35,25 +35,64 @@ namespace FunctionApp1
             string[] files = Directory.GetFiles(@"G:\ZipMade\");
             foreach (string file in files)
             {
-
                 File.Delete(file);
                 //Console.WriteLine($"{file} is deleted.");
             }
 
-         
 
-            WebClient webClient = new WebClient();
 
-            foreach(var file in data1)
-            {
-                webClient.DownloadFile(file.url, @"G:\DownloadAndZip\"+file.Id+"."+file.type);
-            }
+            //WebClient webClient = new WebClient();
+
+            //foreach (var file in data1)
+            //{
+            //    webClient.DownloadFile(file.url, @"G:\DownloadAndZip\" + file.Id + "." + file.type);
+            //}
 
 
             string startpath = @"G:\DownloadAndZip\";
-            string zipPath = @"G:\ZipMade\result.zip";
+            string zipPath = @"G:\ZipMade\SharePoint"+DateTime.Now.ToShortDateString()+".zip";
 
-            ZipFile.CreateFromDirectory(startpath, zipPath);
+            //ZipFile.CreateFromDirectory(startpath, zipPath);
+
+            // Create FileStream for output ZIP archive
+            //using (FileStream zipFile = File.Open("compressed_files.zip", FileMode.Create))
+            //{
+            //    // File to be added to archive
+            //    using (FileStream source1 = File.Open("alice29.txt", FileMode.Open, FileAccess.Read))
+            //    {
+            //        // File to be added to archive
+            //        using (FileStream source2 = File.Open("asyoulike.txt", FileMode.Open, FileAccess.Read))
+            //        {
+            //            using (var archive = new Archive()
+            //            {
+            //                // Add files to the archive
+            //                archive.CreateEntry("alice29.txt", source1);
+            //                archive.CreateEntry("asyoulik3.txt", source2);
+            //                // ZIP the files
+            //                archive.Save(zipFile, new ArchiveSaveOptions() { Encoding = Encoding.ASCII, ArchiveComment = "two files are compressed in this archive" });
+            //            }
+            //        }
+            //    }
+            //}
+
+
+            using (var archive = ZipFile.Open(zipPath, ZipArchiveMode.Create))
+            {
+                //string[] filesToZip = Directory.GetFiles(@"G:\DownloadAndZip\");
+                List<string> filesToZip = new List<string>();
+                foreach(var ff in data1)
+                {
+
+                    filesToZip.AddRange(Directory.GetFiles(@"G:\DownloadAndZip\", ff.Id + "." + ff.type));
+                }
+                
+
+                foreach (string file in filesToZip)
+                {
+                    archive.CreateEntryFromFile(Path.GetFullPath(file), Path.GetFileName(file));
+                }
+            }
+            
 
             //string responseMessage = string.IsNullOrEmpty(name)
             //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
